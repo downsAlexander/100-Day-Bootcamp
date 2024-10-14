6b7d5fd1cde02d8170668f01fbeb60bd1746b1d2
downsAlexander <145305970+downsAlexander@users.noreply.github.com>
2024-10-14 16:29:32 -0400

    Add files via upload

---
+++ Bonus/fizzbuzz.py
@@ -0,0 +1,8 @@
+for n in range(1, 100):
+    if (n % 3 == 0 and n % 5 == 0):
+        print("FizzBuzz")
+    elif n % 5 == 0:
+        print("Buzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    else: print(n)
